f80bb76f198b295a39e0816d1288dd0de1c3c549
Tim Wawrzynczak <twawrzynczak@chromium.org>
2022-01-19 18:35:58 -0700

    clean up combo phy registers
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- common/tigerlake/hw-gfx-gma-combo_phy.adb
+++ common/tigerlake/hw-gfx-gma-combo_phy.adb
@@ -10,79 +10,74 @@ with HW.GFX.GMA.Registers;
 use type HW.Word64;
 
 package body HW.GFX.GMA.Combo_Phy is
-   
+
    type PHY is (PHY_A, PHY_B, PHY_C, PHY_D, PHY_E, PHY_F, PHY_G, PHY_H, PHY_I);
    subtype Combo_Phy is PHY range PHY_A .. PHY_B;
-   
+
    PHY_MISC_DE_TO_IO_COMP_PWR_DOWN	: constant := 1 * 2 ** 23;
    PORT_TX_DW8_ODCC_CLKSEL		: constant := 1 * 2 ** 31;
    PORT_TX_DW8_ODCC_DIV_SEL_MASK	: constant := 3 * 2 ** 29;
-   
-   type Combo_Phy_Reg is (PORT_TX_DW8_LN0,
-			  PORT_TX_DW8_GRP0,
-			  PORT_PCS_DW1_LN0,
-			  PORT_PCS_DW1_GRP0,
-			  PORT_COMP_DW3,
-			  PORT_COMP_DW1,
-			  PHY_MISC,
-			  PORT_COMP_DW0,
-			  PORT_CL_DW5);
-   
-   type Combo_Phy_Regs is array (Combo_Phy_Reg) of Registers_Index;
-   Combo_Phy_Regs_A : constant Combo_Phy_Regs :=
-     (PHY_MISC           => PHY_MISC_A,
-      PORT_CL_DW5        => PORT_CL_DW5_A,
-      PORT_COMP_DW0      => PORT_COMP_DW0_A,
-      PORT_COMP_DW1      => PORT_COMP_DW1_A,
-      PORT_COMP_DW3      => PORT_COMP_DW3_A
-      PORT_TX_DW8_LN0 	 => PORT_TX_DW8_LN0_A,
-      PORT_TX_DW8_GRP0   => PORT_TX_DW8_GRP0_A,
-      PORT_PCS_DW1_LN0   => PORT_PCS_DW1_LN0_A,
-      PORT_PCS_DW1_GRP0  => PORT_PCS_DW1_GRP0_A);
-   Combo_Phy_Regs_B : constant Combo_Phy_Regs :=
-     (PHY_MISC           => PHY_MISC_B,
-      PORT_CL_DW5        => PORT_CL_DW5_B,
-      PORT_COMP_DW0      => PORT_COMP_DW0_B,
-      PORT_COMP_DW1      => PORT_COMP_DW1_B,
-      PORT_COMP_DW3      => PORT_COMP_DW3_B
-      PORT_TX_DW8_LN0 	 => PORT_TX_DW8_LN0_B,
-      PORT_TX_DW8_GRP0   => PORT_TX_DW8_GRP0_B,
-      PORT_PCS_DW1_LN0   => PORT_PCS_DW1_LN0_B,
-      PORT_PCS_DW1_GRP0  => PORT_PCS_DW1_GRP0_B);
-   
+
+   type Regs is array (Combo_Phy) of Registers.Registers_Index;
+   PHY_MISC : constant Regs := Regs'
+     (PHY_A => Registers.PHY_MISC_A,
+      PHY_B => Registers.PHY_MISC_B);
+
+   PORT_CL_DW5 : constant Regs := Regs'
+     (PHY_A => Registers.PORT_CL_DW5_A,
+      PHY_B => Registers.PORT_CL_DW5_B);
+
+   PORT_COMP_DW0 : constant Regs := Regs'
+     (PHY_A => Registers.PORT_COMP_DW0_A,
+      PHY_B => Registers.PORT_COMP_DW0_B);
+
+   PORT_COMP_DW1 : constant Regs := Regs'
+     (PHY_A => Registers.PORT_COMP_DW1_A,
+      PHY_B => Registers.PORT_COMP_DW1_B);
+
+   PORT_COMP_DW3 : constant Regs := Regs'
+     (PHY_A => Registers.PORT_COMP_DW3_A,
+      PHY_B => Registers.PORT_COMP_DW3_B);
+
+   PORT_TX_DW8_LN0 : constant Regs := Regs'
+     (PHY_A => Registers.PORT_TX_DW8_LN0_A,
+      PHY_B => Registers.PORT_TX_DW8_LN0_B);
+
+   PORT_TX_DW8_GRP0 : constant Regs := Regs'
+     (PHY_A => Registers.PORT_TX_DW8_GRP0_A,
+      PHY_B => Registers.PORT_TX_DW8_GRP0_B);
+
+   PORT_PCS_DW1_LN0 : constant Regs := Regs'
+     (PHY_A => Registers.PORT_PCS_DW1_LN0_A,
+      PHY_B => Registers.PORT_PCS_DW1_LN0_B);
+
+   PORT_PCS_DW1_GRP0 : constant Regs := Regs'
+     (PHY_A => Registers.PORT_PCS_DW1_GRP0_A,
+      PHY_B => Registers.PORT_PCS_DW1_GRP0_B);
+
    -- N.B. Several Combo PHY registers come in individual lane registers and
    -- also a group registers. The group register is used exclusively for
    -- programming all of the lane registers with the same value. Reads from
    -- group address are indeterminate, so read from a single lane, then write to
    -- the group.
    procedure Initialize is
-      function Combo_Phy_Reg (Phy : Combo_Phy; 
-			      Reg : Combo_Phy_Reg) return Registers_Index is
-	 Regs : Combo_Phy_Regs;
-      begin
-	 case Phy is
-	    when PHY_A => return Combo_Phy_Regs_A (Reg);
-	    when PHY_B => return Combo_Phy_Regs_B (Reg);
-	 end case;
-      end Combo_Phy_Reg;
-      
       procedure Config_DCC_SusClk (Phy : Combo_Phy) is
 	 DW8 : Word32;
 	 DW1 : Word32;
       begin
 	 --   DCC susclk divider programming
-	 Registers.Read (Combo_Phy_Reg (Phy, PORT_TX_DW8_LN0), DW8);
+	 Registers.Read (PORT_TX_DW8_LN0 (Phy), DW8);
 	 DW8 := DW8 and not PORT_TX_DW8_ODCC_DIV_SEL_MASK;
 	 DW8 := DW8 or PORT_TX_DW8_ODCC_CLKSEL;
-	 Registers.Write (Combo_Phy_Reg (Phy, PORT_TX_DW8_GRP0), DW8);
-	 
+	 Registers.Write (PORT_TX_DW8_GRP0 (Phy), DW8);
+
 	 --   DCC discontinuous mode
-	 Registers.Read (Combo_Phy_Reg (Phy, PORT_PCS_DW1_LN0), DW1);
+	 Registers.Read (PORT_PCS_DW1_LN0 (Phy), DW1);
 	 DW1 := DW1 and not PORT_PCS_DW1_DCC_MODE_SELECT_MASK;  -- 3 * 2 ** 30
 	 DW1 := DW1 or PORT_PCS_DW1_DCC_MODE_SELECT_CONTINUOUS; -- 3 * 2 ** 30
-	 Registers.Write (Combo_Phy_Reg (Phy, PORT_PCS_DW1_GRP0), DW1);
+	 Registers.Write (PORT_PCS_DW1_GRP0 (Phy), DW1);
       end Config_DCC_Susclk;
-      
+
       procedure Config_Procmon_Reference (Phy : Combo_Phy) is
 	 type Procmon_Voltage is (ZERO_85_V, ZERO_95_V, ONE_05_V);
 	 type Procmon_Process is (DOT0, DOT1);
@@ -93,7 +88,7 @@ package body HW.GFX.GMA.Combo_Phy is
 	    DW9     : Word32;
 	    DW10    : Word32;
 	 end record;
-	 
+
 	 ZERO_85_DOT0 : constant Procmon_References :=
 	   (Voltage => ZERO_85_V,
 	    Process => DOT0,
@@ -131,7 +126,7 @@ package body HW.GFX.GMA.Combo_Phy is
 	    PROCESS_MASK : constant := 7 * 2 ** 26;
 	    VOLTAGE_MASK : constant := 3 * 2 ** 24;
 	 begin
-	    Registers.Read (Register => Combo_Phy_DW3_Reg (Phy), DW3);
+	    Registers.Read (Register => PORT_COMP_DW3 (Phy), DW3);
 	    Tmp := DW3 and VOLTAGE_MASK;
 	    case Tmp is
 	       when 16#0000_0000# => Voltage := ZERO_85_V;
@@ -139,14 +134,14 @@ package body HW.GFX.GMA.Combo_Phy is
 	       when 16#0200_0000# => Voltage := ONE_05_V;
 	       when others        => Voltage := ZERO_85_V;
 	    end case;
-	    
+
 	    Tmp := DW3 and PROCESS_MASK;
 	    case Tmp is
 	       when 16#0400_0000# => Process := DOT0;
 	       when 16#0800_0000# => Process := DOT1;
 	       when others =>        Process := DOT0;
 	    end case;
-	    
+
 	    if Process = DOT0 then
 	       if Voltage = ZERO_85_V then
 		  References := ZERO_85_DOT0;
@@ -168,16 +163,16 @@ package body HW.GFX.GMA.Combo_Phy is
 	 References : Procmon_References;
       begin
 	 Read_DW3(Phy, References);
-	 Registers.Unset_And_Set_Mask (Register => Combo_Phy_Reg (Phy, PORT_COMP_DW1),
+	 Registers.Unset_And_Set_Mask (Register => PORT_COMP_DW1 (Phy),
 				        -- not (shift_right(16#ff#, 16) or 16#ff)
 				       Unset_Mask => PORT_COMP_DW1_REF_MASK,
 				       Set_Mask => References.DW1);
-	 Registers.Write (Register => Combo_Phy_Reg (Phy, PORT_COMP_DW9),
+	 Registers.Write (Register => PORT_COMP_DW9 (Phy),
 			  Value    => References.DW9);
-	 Registers.Write (Register => Combo_Phy_Reg (Phy, PORT_COMP_DW10),
+	 Registers.Write (Register => PORT_COMP_DW10 (Phy),
 			  Value    => References.DW10);
       end Config_Procmon_Reference;
-      
+
       function Phy_Is_Master (Phy : Combo_Phy) return Boolean is begin
 	 case Phy of
 	    when PHY_A => return True;
@@ -188,26 +183,25 @@ package body HW.GFX.GMA.Combo_Phy is
       -- Initialize all combo PHYs with Combo PHY DDI Buffer Combo PHY Init Sequence
       for Phy in Combo_Phy'range loop
 	 Config_DCC_SusClk (Phy);
-	 
+
 	 --   Clear PHY_MISC_<DDI that maps to this PHY> DE to IO Comp Pwr Down to 0b.
-	 Registers.Unset_Mask (Combo_Phy_Reg (Phy, PHY_MISC),
-			       PHY_MISC_DE_TO_IO_COMP_PWR_DOWN);
-	 
+	 Registers.Unset_Mask (PHY_MISC (Phy), PHY_MISC_DE_TO_IO_COMP_PWR_DOWN);
+
 	 --   Program procmon reference values in PORT_COMP_DW{1,9,10}
 	 Config_Procmon_Reference (Phy);
-	 
+
 	 --   If this PHY is a comp source, set DW8 irefgen to 1
 	 if Phy_Is_Master (Phy) then
-	    Registers.Set_Mask (Register => Combo_Phy_Reg (Phy, PORT_COMP_DW8),
-				Set_Mask => COMP_DW8_IREFGEN); -- 1 * 2 ** 24
+	    Registers.Set_Mask (Register => PORT_COMP_DW8 (Phy),
+				Set_Mask => PORT_COMP_DW8_IREFGEN); -- 1 * 2 ** 24
 	 end if;
-	 
+
 	 --   Set PORT_COMP_DW0 Comp Init to 1
-	 Registers.Set_Mask (Register => Combo_Phy_Reg (Phy, PORT_COMP_DW0),
+	 Registers.Set_Mask (Register => PORT_COMP_DW0 (Phy),
 			     Set_Mask => PORT_COMP_DW0_COMP_INIT);
-	 
+
 	 --   PORT_CL_DW5 CL Power Down Enable to 1
-	 Registers.Set_Mask (Register => Combo_Phy_Reg (Phy, PORT_CL_DW5),
+	 Registers.Set_Mask (Register => PORT_CL_DW5 (Phy),
 			     Set_Mask => PORT_CL_DW5_POWER_DOWN_ENABLE);
       end loop;
 

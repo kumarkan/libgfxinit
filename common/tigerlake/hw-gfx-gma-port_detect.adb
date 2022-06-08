57ac632e3927ff78fa0eadd0a6daf5f4b77c66b4
Tim Wawrzynczak <twawrzynczak@chromium.org>
2022-06-08 12:48:19 -0600

    common: Begin Tiger Lake integration
    Tiger Lake's integrated graphics engine is built off an entirely new
    igfx architecture named "Iris Xe." This patch adds just enough support
    to compile for Tiger Lake. Fortunately, the backlight programming
    hasn't changed since SKL, so with a little bit of hacking, one can
    also have libgfxinit turn on the backlight to verify something is
    working and running.
    
    The first pass through the support will focus mostly on the eDP panel,
    then Type-C ports (both in fixed/native mode [DP/HDMI] and in Type-C
    DP Alt Mode). TBT will not be mostly ignored in this first pass, as
    it is not as widely used (yet?) as the other protocols.
    
    Tested on google/voxel, with DEBUG_ADA_CODE enabled.
    
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Change-Id: Ic8d32598e5611453ce53f1deaeab981b9f2a952f

---
+++ common/tigerlake/hw-gfx-gma-port_detect.adb
@@ -0,0 +1,44 @@
+--
+-- Copyright (C) Google, LLC
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+
+with HW.GFX.GMA.Config;
+with HW.GFX.GMA.Registers;
+with HW.GFX.GMA.Config_Helpers;
+
+with HW.Debug;
+with GNAT.Source_Info;
+
+package body HW.GFX.GMA.Port_Detect
+is
+   procedure Initialize is
+   begin
+      pragma Debug (Debug.Put_Line (GNAT.Source_Info.Enclosing_Entity));
+   end Initialize;
+
+   procedure Hotplug_Detect
+     (Port     : in Active_Port_Type;
+      Detected : out Boolean)
+   is
+   begin
+      pragma Debug (Debug.Put_Line (GNAT.Source_Info.Enclosing_Entity));
+      Detected := False;
+   end Hotplug_Detect;
+
+   procedure Clear_Hotplug_Detect (Port : Active_Port_Type)
+   is
+   begin
+      pragma Debug (Debug.Put_Line (GNAT.Source_Info.Enclosing_Entity));
+   end Clear_Hotplug_Detect;
+
+end HW.GFX.GMA.Port_Detect;

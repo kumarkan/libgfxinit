d757a5aaf3b085018a61eab3e8a8c4873bbf7190
Tim Wawrzynczak <twawrzynczak@chromium.org>
2022-09-09 10:53:27 -0600

    common/tigerlake: Add combo PHY programming sequence
    This is required before any combo PHY ports can be used, so it will
    be performed early in the modeset.
    
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Change-Id: Id0c2495872250e84057c3130cab19dea05a00b75

---
+++ common/tigerlake/hw-gfx-gma-combo_phy.ads
@@ -0,0 +1,25 @@
+--
+-- Copyright (C) 2022 Google, LLC
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+
+with HW.Debug;
+with GNAT.Source_Info;
+
+use type HW.Word64;
+
+private package HW.GFX.GMA.Combo_Phy is
+
+   procedure Initialize;
+   procedure All_Off;
+
+end HW.GFX.GMA.Combo_Phy;
